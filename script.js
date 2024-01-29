4848f7a2d461a4672d049860d0052305e0f1d74c
Asif <107271220+Asif2902@users.noreply.github.com>
2024-01-29 15:22:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to open the popup
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+// Automatically open the popup when the page loads
+window.onload = function() {
+  openPopup();
+};
